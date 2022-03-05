5b165c7abad8b213dba6b65fc755dbd5fa31099e
Nathan <nathandaud123@gmail.com>
2022-03-05 12:45:23 +0700

    update baru

--- app/src/main/java/user/example/sampah/MainActivity.kt
+++ app/src/main/java/user/example/sampah/MainActivity.kt
@@ -1,11 +1,23 @@
 package user.example.sampah
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import user.example.sampah.ui.login.activity_main3
+
 
 class MainActivity : AppCompatActivity() {
+    private val waktu_loading = 4000
+
+    //4000=4 detik
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Handler().postDelayed({
+            val home = Intent(this@MainActivity,activity_main3::class.java)
+            startActivity(home)
+            finish()
+        }, waktu_loading.toLong())
     }
 }
---
+++ app/src/main/java/user/example/sampah/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package user.example.sampah.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+    val userId: String,
+    val displayName: String
+)
