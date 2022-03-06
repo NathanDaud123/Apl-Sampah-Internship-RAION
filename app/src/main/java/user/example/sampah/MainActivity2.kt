a1eee65f042325ac764e3dc1f7bb0d8dcdd023da
haidarniv <711haidarhanif@gmail.com>
2022-03-06 14:13:21 +0700

    Percobaan image slider 1. Nambah dec... di gradle 2. nambah adapter.kt 3. nambah viewpage di main 2 xml 3. nambah codingan di main 2 kt

--- app/src/main/java/user/example/sampah/MainActivity2.kt
+++ app/src/main/java/user/example/sampah/MainActivity2.kt
@@ -3,9 +3,31 @@ package user.example.sampah
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
+
 class MainActivity2 : AppCompatActivity() {
+
+    lateinit var adapters: Adapters
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        var list = mutableListOf<Int>()
+
+        list.add(R.drawable.rectangle_1)
+        list.add(R.drawable.facebook)
+        list.add(R.drawable.rectangle_1)
+        list.add(R.drawable.gog)
+        list.add(R.drawable.rectangle_1)
+        list.add(R.drawable.pemuda_panjasila)
+
+
+        adapters=Adapters(this)
+        adapters.setContentList(list)
+        viewpager.adapter=adapters
+
+
+
     }
 }
