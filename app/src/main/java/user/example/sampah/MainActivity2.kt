b5f166e0538b49854aa806f5c222bb347f00c416
haidarniv <711haidarhanif@gmail.com>
2022-03-11 21:23:23 +0700

    Main Activity2 : - Sesuaiin dengan prototype baru (gambar dan teks) - button transisi ke actvy 3 juga udah ada
    Main Activity 4 :
    - Sesuaiin prototype baru
    - Tombol back udah ada

--- app/src/main/java/user/example/sampah/MainActivity2.kt
+++ app/src/main/java/user/example/sampah/MainActivity2.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.viewpager2.widget.ViewPager2
 import androidx.activity.result.contract.ActivityResultContracts
 
@@ -15,34 +17,35 @@ class MainActivity2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
-        var list = mutableListOf<Int>()
+        //KODE TEKS CHANGE DISPLAY VIA BUTTON
+        var lanjut = findViewById<TextView>(R.id.ButtonLanjut)
+        var teksganti = findViewById<TextView>(R.id.TeksGantiganti)
+        var judulganti = findViewById<TextView>(R.id.mainJudulgantiganti)
+        var gambar1 = findViewById<ImageView>(R.id.page2gambar1)
+        var itung: Int = 0
 
-        list.add(R.drawable.rectangle_1)
-        list.add(R.drawable.facebook)
-        list.add(R.drawable.rectangle_1)
-        list.add(R.drawable.gog)
-        list.add(R.drawable.rectangle_1)
-        list.add(R.drawable.pemuda_panjasila)
-
-
-        adapters=Adapters(this)
-        adapters.setContentList(list)
-        val viewpager = findViewById<ViewPager2>(R.id.viewpager)
-        viewpager.adapter=adapters
-
-
-
-        var lewati = findViewById<Button>(R.id.lewati)
-        lewati.setOnClickListener{
-            var lanjut3 = Intent(this,MainActivity3::class.java)
-            startActivity(lanjut3)
-
-
-
-
-
-
-
+        lanjut.setOnClickListener {
+            if (itung == 0) {
+                teksganti.text = "Sampah akan diambil oleh kolektor kami sesuai alamat anda."
+                judulganti.text = "Tunggu Kolektor"
+                gambar1.setImageResource(R.drawable.page2gambar2)
+                itung++
+            } else if (itung == 1){
+                teksganti.text = "Tukarkan sampah yang sudah anda kumpulkan menjadi uang yang dapat diperbelanjakan."
+                judulganti.text = "Dapatkan Uang"
+                lanjut.text = "Selesai"
+                gambar1.setImageResource(R.drawable.page2gambar3)
+                itung++
+            } else if (itung == 2) {
+                var nextPage = Intent(this,MainActivity3::class.java)
+                startActivity(nextPage)
+            }
         }
+
+//        var lewati = findViewById<Button>(R.id.lewati)
+//        lewati.setOnClickListener{
+//            var lanjut3 = Intent(this,MainActivity3::class.java)
+//            startActivity(lanjut3)
+//        }
     }
 }
--- app/src/main/java/user/example/sampah/MainActivity4.kt
+++ app/src/main/java/user/example/sampah/MainActivity4.kt
@@ -1,11 +1,32 @@
 package user.example.sampah
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
-class MainActivity4 : AppCompatActivity() {
+class MainActivity4 : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var MasukButtonpage4 : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main4)
+
+        MasukButtonpage4 = findViewById(R.id.MasukButtonpage4)
+
+        MasukButtonpage4.setOnClickListener(this)
+
+    }
+
+    override fun onClick(p0: View) {
+        when(p0.id) {
+            R.id.MasukButtonpage4 -> {
+                val intenBiasa = Intent(this@MainActivity4, MainActivity3::class.java)
+                startActivity(intenBiasa)
+            }
+
+        }
     }
 }
