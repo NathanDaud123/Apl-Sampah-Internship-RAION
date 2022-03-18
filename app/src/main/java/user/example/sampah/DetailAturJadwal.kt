1725e6461374d9b487c49baa19ebb266e80958ba
Nathan <nathandaud123@gmail.com>
2022-03-18 23:59:20 +0700

    tambah detail hari ini & atur jadwal

---
+++ app/src/main/java/user/example/sampah/DetailAturJadwal.kt
@@ -0,0 +1,11 @@
+package user.example.sampah
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailAturJadwal : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_atur_jadwal)
+    }
+}
