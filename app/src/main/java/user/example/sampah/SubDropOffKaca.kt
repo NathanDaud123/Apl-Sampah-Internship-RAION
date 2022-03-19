370e008d9da854eb13a1ee9f20f547c0ce91af9a
haidarniv <711haidarhanif@gmail.com>
2022-03-19 23:20:57 +0700

    sabtu malem finish drop off

---
+++ app/src/main/java/user/example/sampah/SubDropOffKaca.kt
@@ -0,0 +1,11 @@
+package user.example.sampah
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SubDropOffKaca : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sub_drop_off_kaca)
+    }
+}
